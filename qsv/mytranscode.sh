02f76af47b4b94f07b59fc770a8120e8a16495f2
Sean Johnson <sean@ttys0.net>
2021-01-24 20:49:22 -0600

    added quotes around

--- qsv/mytranscode.sh
+++ qsv/mytranscode.sh
@@ -6,7 +6,7 @@ find ${SRC_PATH} -type f -name "*.mkv" -print0 | while IFS= read -r -d '' FILE; 
 
   CODEC=$(/usr/local/bin/ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=noprint_wrappers=1:nokey=1 "${FILE}")
 
-  if [ ${CODEC} = "vc1" ]; then
+  if [ "${CODEC}" = "vc1" ]; then
     /usr/local/bin/other-transcode --eac3 --pass-dts --main-audio 1=stereo --add-subtitle auto --burn-subtitle auto --preset veryslow "${FILE}" < /dev/null
   else
     /usr/local/bin/other-transcode --decode all --qsv-decoder --eac3 --pass-dts --main-audio 1=stereo --add-subtitle auto --burn-subtitle auto --preset veryslow "${FILE}" < /dev/null
